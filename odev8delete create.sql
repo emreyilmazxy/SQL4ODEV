4e67f8a3d5c5ecae6d6d4a86cce2074e5765dcfc
Emre Yılmaz <emreyilmazxy@gmail.com>
2025-06-06 20:14:49 +0300

    Add files via upload

---
+++ odev8delete create.sql
@@ -0,0 +1,22 @@
+--UPDATE employee 
+--SET name = 'Alberta'
+--WHERE id = 6 
+
+--UPDATE employee 
+--SET birtday = '1995-06-06'
+ --     email = 'aaa@gmail.com'
+--WHERE name = 'Cliff' 
+
+
+--UPDATE employee 
+--SET name = 'Linda'
+--WHERE birtday = '1965-06-01' 
+--RETURNING * ;
+
+
+--DELETE FROM employee
+--Where id =11;
+
+--DELETE FROM employee
+--Where name ='Scarlett'
+--returning *;
